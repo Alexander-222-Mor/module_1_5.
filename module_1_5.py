0bf51a39f4f9a7b558d814e54fffa9b18d47e92d
Alexander-222-Mor <amorgunov948@gmail.com>
2024-10-30 13:46:08 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (2, 6, False, 'string', 3 )
+print (immutable_var)
+# immutable_var[2] = 32: не изменяемый тип
+mutable_list= [4, 6, True, 'list']
+print (mutable_list)
+mutable_list[3] = 7
+print (mutable_list)
